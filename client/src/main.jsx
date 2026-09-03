421a5434ea8f6aacae7dcb9618a12f27c5601cd5
Saurabh Maurya <srbmaury@gmail.com>
2026-09-03 20:11:00 +0530

    Provide hiring organization context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { OrganizationProvider } from "./context/OrganizationContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
@@ -15,7 +16,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <NotificationProvider>
         <BrowserRouter>
           <AuthProvider>
-            <App />
+            <OrganizationProvider>
+              <App />
+            </OrganizationProvider>
           </AuthProvider>
         </BrowserRouter>
       </NotificationProvider>
