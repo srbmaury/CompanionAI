d3f305cb09bc230f18d9286e6ba2868dc4f070f6
Saurabh Maurya <srbmaury@gmail.com>
2026-09-04 06:14:05 +0530

    Fix Vite SEO config lint

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { mkdir, writeFile } from "node:fs/promises";
 import path from "node:path";
+import process from "node:process";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
